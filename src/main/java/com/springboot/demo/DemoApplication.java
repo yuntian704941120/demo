1c792c23e3655de05590345f6d8ce7febc9dca8e
wuyuntian <wuyuntian@vipmro.com.cn>
2019-03-13 18:52:06 +0800

    fix:添加

--- src/main/java/com/springboot/demo/DemoApplication.java
+++ src/main/java/com/springboot/demo/DemoApplication.java
@@ -1,6 +1,8 @@
 package com.springboot.demo;
 
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +25,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 
-
+@EnableAdminServer
 @RestController
 @SpringBootApplication(scanBasePackages = "com.springboot.demo")
 @EnableAutoConfiguration
@@ -30,6 +37,48 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	/**
+	 * dev 环境加载
+	 */
+	@Profile("dev")
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
+	}
+
+	/**
+	 * prod 环境加载
+	 */
+	@Profile("prod")
+	@Configuration
+	public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+		private final String adminContextPath;
+
+		public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+			this.adminContextPath = adminServerProperties.getContextPath();
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+			successHandler.setTargetUrlParameter("redirectTo");
+
+			http.authorizeRequests()
+					.antMatchers(adminContextPath + "/assets/**").permitAll()
+					.antMatchers(adminContextPath + "/login").permitAll()
+					.anyRequest().authenticated()
+					.and()
+					.formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+					.logout().logoutUrl(adminContextPath + "/logout").and()
+					.httpBasic().and()
+					.csrf().disable();
+		}
 	}
 
 
